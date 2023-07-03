866159f9a6cd9084515fb1d32044a34c688729c6
VyHuynh <vykhanghuynh@gmail.com>
2023-07-03 09:04:22 +0700

    Update

--- api/server.js
+++ api/server.js
@@ -1,30 +1,21 @@
-var jsonServer = require("json-server");
-var server = jsonServer.create();
-var router = jsonServer.router("db.json");
-var middlewares = jsonServer.defaults();
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
-// Thêm dòng này để cho phép gửi dữ liệu JSON qua body
-server.use(jsonServer.bodyParser);
-
-server.use(function (req, res, next) {
-  if (
-    req.method === "POST" ||
-    req.method === "PUT" ||
-    req.method === "PATCH" ||
-    req.method === "DELETE"
-  ) {
-    // Chuyển đổi các phương thức POST, PUT, PATCH, DELETE thành GET và di chuyển dữ liệu sang query params
-    // Điều này sẽ khiến JSON Server hiểu rằng đó là yêu cầu GET
-    req.method = "GET";
-    req.query = req.body;
-  }
-  // Tiếp tục đến router của JSON Server
-  next();
-});
-
+// Add this before server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
 server.use(router);
-server.listen(3000, function () {
+server.listen(3000, () => {
   console.log("JSON Server is running");
 });
+
+// Export the Server API
+module.exports = server;
